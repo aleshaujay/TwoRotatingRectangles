a2c1189ee9a78c66cbdf7b33afb6b598c55290fd
aleshaujay <aleshaujay@icloud.com>
2024-07-24 02:39:54 +0700

    Initial Commit

---
+++ TwoRotatingRectangles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoRotatingRectangles
+//
+//  Created by Lex Koby on 24.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
